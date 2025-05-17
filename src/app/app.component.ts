29be3564b79ac32857cf96b7b4f6d0ce01ed4001
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-05-16 20:40:19 -0400

    Creating the shell to start playing with signals and state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { LeftSideComponent } from "./left-side/left-side.component";
+import { RightSideComponent } from "./right-side/right-side.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, LeftSideComponent, RightSideComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'refresher';
+  title = "refresher";
 }
