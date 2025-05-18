e664ecd15ed653140c9a970d23b81ffd32b42139
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-05-17 22:59:37 -0400

    Simple form that maybe I can use

--- src/app/left-side/left-side.component.ts
+++ src/app/left-side/left-side.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-left-side',
-  imports: [],
-  templateUrl: './left-side.component.html',
-  styleUrl: './left-side.component.scss'
+  selector: "app-left-side",
+  imports: [ReactiveFormsModule],
+  templateUrl: "./left-side.component.html",
+  styleUrl: "./left-side.component.scss",
 })
 export class LeftSideComponent {
+  nameForm = new FormGroup({
+    name: new FormControl("", Validators.required),
+  });
 
+  handleSubmit() {
+    console.log(this.nameForm.value.name);
+  }
 }
